60f33419f8360f0d91e4ce3754bc32877ee740d0
Jesse Zhao <jessez.chabot@gmail.com>
2022-02-22 20:23:18 -0800

    Implement multithreading

--- scripts/data_fetcher.py
+++ scripts/data_fetcher.py
@@ -1,13 +1,16 @@
 import pandas as pd
 import requests
+from requests.structures import CaseInsensitiveDict
+from concurrent.futures import ThreadPoolExecutor
 from tqdm import tqdm
 
-api_key = 'OV8HxsEXoIqdA3tr'
+api_key = ''
 data_dir = 'data/'
-checkpoint_dir = '.script_checkpoints/e_above_hull.pkl'
-
-properties = ['e_above_hull']
+checkpoint_dir = '.script_checkpoints/'
 file_name = "e_above_hull"
+
+endpoint = 'thermo'
+properties = ['energy_above_hull']
 
 dict = {'id' : []}
 for property in properties:
@@ -15,24 +18,35 @@ for property in properties:
 
 ids = pd.read_csv(data_dir + 'ids.csv')
 
-for count, i in enumerate(tqdm(ids.id)):
+pbar = tqdm(total=len(ids))
+def fetch(url, headers, i, count, fails, key):
     try:
-        res =  requests.get(f"https://www.materialsproject.org/rest/v2/materials/{i}/vasp?API_KEY={api_key}")
+        res =  requests.get(url, headers=headers)
         data = res.json()
-        dict['id'].append(i)
         
         for property in properties:
-            dict[property].append(data["response"][0][property])
-            
+            dict[property].append(data[key][0][property])
+        dict['id'].append(i)
     except Exception as e:
-        print(f"{e} on {ids}")
+        fails.append(i)
         dict['id'].append(i)
         for property in properties:
             dict[property].append(None)
-    
+
     if count % 1000 == 0:
         df = pd.DataFrame(dict)
         df.to_pickle(checkpoint_dir + file_name + ".pkl")
+    pbar.update(1)
+
+headers = CaseInsensitiveDict()
+headers["accept"] = "application/json"
+headers["X-API-KEY"] = api_key
+fails = []
+with ThreadPoolExecutor(max_workers=10) as executor:
+    for count, i in enumerate(ids.id):
+        executor.submit(fetch, f"https://api.materialsproject.org/{endpoint}/{i}/?all_fields=true", headers, i, count, fails, "data")
+pbar.close()
 
 df = pd.DataFrame(dict)
 df.to_csv(data_dir + file_name + '.csv')
+print(fails)
